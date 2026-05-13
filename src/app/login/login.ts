566078fe34a2aea41fc014b6485449238d78fe1c
adding files <abhishek.20ec106@jssaten.ac.in>
2026-05-13 17:26:37 +0530

    property binding

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
-  title = 'Login';
+  imageUrl = "https://i.pinimg.com/736x/10/66/46/1066461b5705cbd2a4a074dfa7df12e9.jpg";
+  isDisabled = false;
+  username = "Abhishek";
+  isActive = false;
 }
--- src/app/signin/sigin.ts
+++ src/app/signin/sigin.ts
@@ -2,10 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-signin',
-    template: `<h2>Sign In</h2>`,
-    styles: [`
-        h2{
-        color: red;}`]
+    templateUrl: './signin.html',
+    styleUrls: ['./signin.css'],
+    // template: `<h2>Sign In</h2>`,
+    // styles: [`
+    //     h2{
+    //     color: red;}`]
 })
 
 export class SignInComponent {
